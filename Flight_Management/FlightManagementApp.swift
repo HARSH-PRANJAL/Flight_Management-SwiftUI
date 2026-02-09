829cb6051f65043695093ff3ce1867247a631850
harsh <harshprnajal17030@gmail.com>
2026-02-09 14:16:21 +0530

    Created initial model context for the app

--- Flight_Management/FlightManagementApp.swift
+++ Flight_Management/FlightManagementApp.swift
@@ -1,9 +1,12 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct FlightManagementApp: App {
     var body: some Scene {
         WindowGroup {
+            ContentView()
+                .modelContainer(for: [Route.self, RouteNode.self, Airport.self, Trip.self, TripNodeStatus.self, Aircraft.self, Staff.self], isAutosaveEnabled: true)
         }
     }
 }
