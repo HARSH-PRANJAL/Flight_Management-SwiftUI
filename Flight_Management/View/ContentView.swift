16524e37471a2d0eb5b246937f46771e83d8324b
harsh <harshprnajal17030@gmail.com>
2026-02-11 22:47:19 +0530

    Moved form components to util

--- Flight_Management/View/ContentView.swift
+++ Flight_Management/View/ContentView.swift
@@ -1,9 +1,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var addStaff = false
+    @State var addAirport = false
+    @State var addAircraft = false
+    @State var addRoute = false
+    
     var body: some View {
         NavigationStack {
-            StaffListView()
-        }
+                Button(action: {
+                    addStaff = true
+                }, label: {
+                    Text("Add Staff")
+                })
+                Button(action: {
+                    addAirport = true
+                }, label: {
+                    Text("Add Airport")
+                })
+                Button(action: {
+                    addAircraft = true
+                }, label: {
+                    Text("Add Aircraft")
+                })
+                Button(action: {
+                    addRoute = true
+                }, label: {
+                    Text("Add Route")
+                })
+            }
+            .sheet(isPresented: $addStaff, content: {
+                StaffRegistrationForm()
+            })
+            .sheet(isPresented: $addAirport, content: {
+                AirportRegistrationForm()
+            })
+            .sheet(isPresented: $addAircraft, content: {
+                AircraftRegistrationForm()
+            })
+            .sheet(isPresented: $addRoute, content: {
+                RouteRegistrationForm()
+            })
     }
 }
