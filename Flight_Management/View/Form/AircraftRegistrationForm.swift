4c981d4b24f8ea5970018069cafc62325150ba36
harsh <harshprnajal17030@gmail.com>
2026-02-17 15:15:55 +0530

    Fixed multiple notification overlapping in aircraft registration form

--- Flight_Management/View/Form/AircraftRegistrationForm.swift
+++ Flight_Management/View/Form/AircraftRegistrationForm.swift
@@ -5,28 +5,22 @@ struct AircraftRegistrationForm: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) var context
 
+    @Binding var isSheetVisible: Bool
     @State private var viewModel = AircraftRegistrationFormViewModel()
 
     var body: some View {
         ZStack {
             Color(.systemGroupedBackground).ignoresSafeArea(.all)
-            AircraftRegistrationContent(viewModel: viewModel)
+            AircraftRegistrationContent(viewModel: viewModel, isPresented: $isSheetVisible)
         }
         .navigationTitle("Aircraft Registration")
         .navigationBarTitleDisplayMode(.inline)
-        .overlay {
-            if viewModel.submissionState == .error {
-                ErrorOverlay(message: "Aircraft can not be registered.")
-            } else if viewModel.submissionState == .success {
-                SuccessOverlay(message: "Aircraft registered.")
-            }
-        }
     }
 }
 
 #Preview {
     NavigationStack {
-        AircraftRegistrationForm()
+        AircraftRegistrationForm(isSheetVisible: .constant(false))
             .modelContainer(for: Aircraft.self, inMemory: true)
     }
 }
