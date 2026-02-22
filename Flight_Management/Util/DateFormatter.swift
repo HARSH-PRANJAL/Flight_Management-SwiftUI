9798738d99afde84799f69340fbaea12e0977f4c
harsh <harshprnajal17030@gmail.com>
2026-02-22 21:17:43 +0530

    Project update 1. renamed multiple files

--- Flight_Management/Util/DateFormatter.swift
+++ Flight_Management/Util/DateFormatter.swift
@@ -1,17 +1,18 @@
 import Foundation
 
-func formatDate(_ date: Date,
-                format: String,
-                timeZone: TimeZone? = nil,
-                locale: Locale = .current) -> String {
-    
+func formatDate(
+    _ date: Date,
+    format: String,
+    timeZone: TimeZone? = nil,
+    locale: Locale = .current
+) -> String {
     let formatter = DateFormatter()
     formatter.dateFormat = format
     formatter.locale = locale
-    
+
     if let timeZone {
         formatter.timeZone = timeZone
     }
-    
+
     return formatter.string(from: date)
 }
