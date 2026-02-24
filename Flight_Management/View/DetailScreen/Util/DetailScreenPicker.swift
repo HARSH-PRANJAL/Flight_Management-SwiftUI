8434e383c6dac6e88a13f57b9bd639b635657bc2
harsh <harshprnajal17030@gmail.com>
2026-02-24 14:22:36 +0530

    Fixed padding in detil screen picker

--- Flight_Management/View/DetailScreen/Util/DetailScreenPicker.swift
+++ Flight_Management/View/DetailScreen/Util/DetailScreenPicker.swift
@@ -5,12 +5,12 @@ enum DetailTab: String, CaseIterable {
     case tripHistory = "Trips"
 }
 
-func detailScreenPicker<T>(selectedTab: Binding<T>) -> some View where T: CaseIterable & Hashable & RawRepresentable, T.RawValue == String {
+func detailScreenPicker<T>(selectedTab: Binding<T>) -> some View
+where T: CaseIterable & Hashable & RawRepresentable, T.RawValue == String {
     Picker("View Mode", selection: selectedTab) {
         ForEach(Array(T.allCases), id: \.self) { tab in
-            Text(tab.rawValue).tag(tab)
+            Text(" \(tab.rawValue)    ").tag(tab)
         }
     }
     .pickerStyle(.segmented)
-    .padding(.horizontal, 16)
 }
